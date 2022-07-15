ab4eac7bab31b1511c3844219a06da29b2c9d051
Irina <ira.nesheret@yandex.ru>
2022-07-15 22:41:00 +0300

    Добавили решение задачи

---
+++ Ex_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и выдает максимальное из этих чисел.
+a = 5; b = 7 -> max = 7
+a = 2; b = 10 -> max = 10
+a = -9; b = -3 -> max = -3
+*/
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()); // Первое число
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()); // Второе число
+
+if (a > b)
+{
+    Console.WriteLine ("Максимальное число" + a);
+}
+else
+{
+    Console.WriteLine ("Максимальное число" + b);
+}
